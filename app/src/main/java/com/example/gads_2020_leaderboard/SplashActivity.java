fdd1ce8ca116110635c8858de1a4fa75cedbcaf1
Elikem Bansah <elikembansah27@gmail.com>
2020-09-09 11:16:27 +0000

    Initial commit

---
+++ app/src/main/java/com/example/gads_2020_leaderboard/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.gads_2020_leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT;
+
+    static {
+        SPLASH_TIME_OUT = 4000;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent leaderboardIntent = new Intent(SplashActivity.this, LeaderboardActivity.class);
+                startActivity(leaderboardIntent);
+                finish();
+
+            }
+
+        },SPLASH_TIME_OUT);
+    }
+}
