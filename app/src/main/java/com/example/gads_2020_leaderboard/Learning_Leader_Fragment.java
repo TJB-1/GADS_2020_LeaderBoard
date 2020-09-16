2326c61c582c30a66fb6e29c54439e92a0ea7593
Elikem Bansah <elikembansah27@gmail.com>
2020-09-16 13:46:53 +0000

    Code Update

---
+++ app/src/main/java/com/example/gads_2020_leaderboard/Learning_Leader_Fragment.java
@@ -0,0 +1,26 @@
+package com.example.gads_2020_leaderboard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+public class Learning_Leader_Fragment extends FragmentActivity {
+
+    View mView;
+
+    public Learning_Leader_Fragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+
+//        mView = inflater.inflate(R.layout.learning_leader, container, false);
+        return mView;
+    }
+}
