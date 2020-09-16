6fb764d53d311f7b4e9bc90715c7aadc8e1ab0bb
Elikem Bansah <elikembansah27@gmail.com>
2020-09-16 13:45:19 +0000

    Code Update

--- app/src/main/java/com/example/gads_2020_leaderboard/LeaderboardActivity.java
+++ app/src/main/java/com/example/gads_2020_leaderboard/LeaderboardActivity.java
@@ -3,12 +3,18 @@ package com.example.gads_2020_leaderboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_id);
+        setSupportActionBar(mToolbar);
     }
 }
